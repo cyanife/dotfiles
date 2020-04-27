577cf5f5b9e17555a354e165cae2c3940610cb4a
chu <chu@skysense.co.jp>
2020-04-27 18:30:48 +0900

    zsh

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env zsh
 
-ZINIT_HOME="${$HOME/.zinit}"
+# requirements
+print -P "%F{33}▓▒░ %F{220}Checking for requirements…%f"
+if ! [ -x "$(command -v git)" ]; then
+    echo 'git is not installed.' >&2
+    exit 1
+fi
+if ! [ -x "$(command -v svn)" ]; then
+    echo 'subversion is not installed.' >&2
+    exit 1
+fi
+if ! [ -x "$(command -v unzip)" ]; then
+    echo 'unzip is not installed.' >&2
+    exit 1
+fi
+if ! [ -x "$(command -v lua)" ]; then
+    echo 'lua is not installed.' >&2
+    exit 1
+fi
+
+ZINIT_HOME="$HOME/.zinit"
 ZINIT_BIN="bin"
 
 if [[ ! -f $ZINIT_HOME/$ZINIT_BIN/zinit.zsh ]]; then
     print -P "%F{33}▓▒░ %F{220}Installing zdharma/zinit…%f"
-    command mkdir -p "$ZINIT_HOME" && command chmodi g-rwX "$ZINIT_HOME"
+    command mkdir -p "$ZINIT_HOME" && command chmod g-rwX "$ZINIT_HOME"
     command git clone https://github.com/zdharma/zinit "$ZINIT_HOME/$ZINIT_BIN"
     if [ $? -eq 0 ]; then
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f"
@@ -16,6 +35,8 @@ if [[ ! -f $ZINIT_HOME/$ZINIT_BIN/zinit.zsh ]]; then
     fi
 fi
 
-sudo apt install --yes subversion unzip
-compaudit | xargs sudo chmod g-w,o-w
-
+print -P "%F{33}▓▒░ %F{220}Detecting & fixing permission problems…%f"
+if [ -x "$(command -v compaudit)" ]; then
+    compaudit | xargs sudo chmod g-w,o-w
+fi
+print -P "%F{33}▓▒░ %F{34}All Done!.%f"
