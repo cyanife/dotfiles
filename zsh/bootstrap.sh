b7a59c3a8d5f02658305627c87d478582e86a3f6
cyanife <cyanife@gmail.com>
2022-10-09 03:12:40 +0900

    update zsh config

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -19,24 +19,5 @@ if ! [ -x "$(command -v lua)" ]; then
     exit 1
 fi
 
-ZINIT_HOME="$HOME/.zinit"
-ZINIT_BIN="bin"
-
-if [[ ! -f $ZINIT_HOME/$ZINIT_BIN/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing zdharma/zinit…%f"
-    command mkdir -p "$ZINIT_HOME" && command chmod g-rwX "$ZINIT_HOME"
-    command git clone https://github.com/zdharma/zinit "$ZINIT_HOME/$ZINIT_BIN"
-    if [ $? -eq 0 ]; then
-        print -P "%F{33}▓▒░ %F{34}Installation successful.%f"
-        echo -e "source $ZINIT_HOME/$ZINIT_BIN/zinit.zsh" >> $HOME/.zshrc
-    else
-        print -P "%F{160}▓▒░ The clone has failed.%f"
-        exit 1
-    fi
-fi
-
-print -P "%F{33}▓▒░ %F{220}Detecting & fixing permission problems…%f"
-if [ -x "$(command -v compaudit)" ]; then
-    compaudit | xargs sudo chmod g-w,o-w
-fi
-print -P "%F{33}▓▒░ %F{34}All Done!.%f"
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+echo -e "[ -f ~/.zshrc.local ] && source ~/.zshrc.local" >> $HOME/.zshrc
